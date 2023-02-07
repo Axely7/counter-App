c1e7f89a81497c1af639b342419af009ab268a39
Axel Jimenez <axely7@hotmail.com>
2023-02-07 09:03:03 -0600

    counter for ios and android

--- src/components/Fab.tsx
+++ src/components/Fab.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
-import {TouchableOpacity, View, Text, StyleSheet} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableNativeFeedback,
+  TouchableOpacity,
+  Platform,
+} from 'react-native';
 
 interface Props {
   title: string;
@@ -8,24 +15,52 @@ interface Props {
 }
 
 export const Fab = ({title, onPress, position = 'br'}: Props) => {
-  return (
-    <TouchableOpacity style={styles.fabLocationBR} onPress={onPress}>
-      <View style={styles.fab}>
-        <Text style={styles.fabText}>{title}</Text>
+  const ios = () => {
+    return (
+      <TouchableOpacity
+        onPress={onPress}
+        activeOpacity={0.8}
+        style={[
+          styles.fabLocation,
+          position === 'bl' ? styles.left : styles.right,
+        ]}>
+        <View style={styles.fab}>
+          <Text style={styles.fabText}>{title}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
+  const android = () => {
+    return (
+      <View
+        style={[
+          styles.fabLocation,
+          position === 'bl' ? styles.left : styles.right,
+        ]}>
+        <TouchableNativeFeedback
+          onPress={onPress}
+          background={TouchableNativeFeedback.Ripple('#28425B', false, 30)}>
+          <View style={styles.fab}>
+            <Text style={styles.fabText}>{title}</Text>
+          </View>
+        </TouchableNativeFeedback>
       </View>
-    </TouchableOpacity>
-  );
+    );
+  };
+
+  return Platform.OS === 'ios' ? ios() : android();
 };
 
 const styles = StyleSheet.create({
-  fabLocationBR: {
+  fabLocation: {
     position: 'absolute',
     bottom: 25,
+  },
+  right: {
     right: 25,
   },
-  fabLocationBL: {
-    position: 'absolute',
-    bottom: 25,
+  left: {
     left: 25,
   },
   fab: {
@@ -34,6 +69,15 @@ const styles = StyleSheet.create({
     height: 60,
     borderRadius: 100,
     justifyContent: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 4.65,
+
+    elevation: 8,
   },
   fabText: {
     color: 'white',

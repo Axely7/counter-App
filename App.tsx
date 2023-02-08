b6df46ac40e9722537721efc128eadc118bc6602
Axel Jimenez <axely7@hotmail.com>
2023-02-08 10:11:54 -0600

    practicing flex box, object model, position

--- App.tsx
+++ App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import {CounterScreen} from './src/screens/CounterScreen';
+import {SafeAreaView} from 'react-native';
+// import {BoxObjectModelScreen} from './src/screens/BoxObjectModelScreen';
+import {DimensionesScreen} from './src/screens/DimensionesScreen';
+// import {CounterScreen} from './src/screens/CounterScreen';
 // import {HolaMundoScreen} from './src/screens/HolaMundoScreen';
 
 const App = () => {
   // return <HolaMundoScreen />;
-  return <CounterScreen />;
+  // return <CounterScreen />;
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      {/* <BoxObjectModelScreen /> */}
+      <DimensionesScreen />
+    </SafeAreaView>
+  );
 };
 
 export default App;

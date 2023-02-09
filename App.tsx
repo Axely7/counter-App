9424415a28fd9fceb1b9dd09002935af3aab63e1
Axel Jimenez <axely7@hotmail.com>
2023-02-09 09:43:01 -0600

    playing with postion absolute and relative

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native';
 // import {BoxObjectModelScreen} from './src/screens/BoxObjectModelScreen';
-import {DimensionesScreen} from './src/screens/DimensionesScreen';
+// import {DimensionesScreen} from './src/screens/DimensionesScreen';
+import {PositionScreen} from './src/screens/PositionScreen';
 // import {CounterScreen} from './src/screens/CounterScreen';
 // import {HolaMundoScreen} from './src/screens/HolaMundoScreen';
 
@@ -11,7 +12,8 @@ const App = () => {
   return (
     <SafeAreaView style={{flex: 1}}>
       {/* <BoxObjectModelScreen /> */}
-      <DimensionesScreen />
+      {/* <DimensionesScreen /> */}
+      <PositionScreen />
     </SafeAreaView>
   );
 };
